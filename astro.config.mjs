ce0cdb66495ed23340e4e96c8b2368730cc923c7
Te3sk <nicco.fulgaro@outlook.it>
2022-08-29 20:00:41 +0200

    tryng to move all things

---
+++ astro.config.mjs
@@ -0,0 +1,52 @@
+import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+
+export default {
+  // ...
+  integrations: [tailwind()],
+}
+
+// https://astro.build/config
+// export default defineConfig({});
+
+
+// import path from "path";
+// import { fileURLToPath } from "url";
+
+// import { defineConfig } from "astro/config.js";
+
+// import tailwind from "@astrojs/tailwind";
+// import sitemap from "@astrojs/sitemap";
+// import image from "@astrojs/image";
+
+// import { SITE } from "./src/config.mjs";
+
+// const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// // https://astro.build/config
+// export default defineConfig({
+//   // Astro uses this full URL to generate your sitemap and canonical URLs in your final build
+//   site: SITE.domain,
+//   base: "/",
+
+//   output: "static",
+
+//   integrations: [
+//     tailwind({
+//       config: {
+//         applyBaseStyles: false,
+//       },
+//     }),
+//     sitemap(),
+//     image(),
+//   ],
+
+//   vite: {
+//     resolve: {
+//       alias: {
+//         "~": path.resolve(__dirname, "./src"),
+//       },
+//     },
+//   },
+// });
